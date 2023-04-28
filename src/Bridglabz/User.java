65d21c2f96e5cff7683d5466486d91bca97ccaee
nehanarta <nehabaswanthrao@gmail.com>
2023-04-28 15:34:59 +0530

    Added:User first name

--- src/Bridglabz/User.java
+++ src/Bridglabz/User.java
@@ -2,7 +2,17 @@ package Bridglabz;
 import java.util.regex.*;
 
 public class User {
+    static Pattern pattern=Pattern.compile("^[A-Z][a-z]{3,}");
     public static void main(String[] args){
-        System.out.println("welcome to the regex");
+            System.out.println("welcome to regex program");
+            String firstName="Neha";
+            Matcher matcher = pattern.matcher(firstName);
+            if (matcher.matches()) {
+                System.out.println("valid");
+            } else {
+                System.out.println("invalid");
+            }
+            System.out.println(firstName);
+        }
     }
-}
+
