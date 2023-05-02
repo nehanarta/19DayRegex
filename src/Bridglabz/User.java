e620a9a816bc01bc48721ee1aecf576701d2fb3d
Dell <nehabaswantrao@gmail.com>
2023-05-02 19:24:22 +0530

    mobileNo

--- src/Bridglabz/User.java
+++ src/Bridglabz/User.java
@@ -2,17 +2,17 @@ package Bridglabz;
 import java.util.regex.*;
 
 public class User {
-    static Pattern pattern=Pattern.compile("^(.+)@(.+)$");
+    static Pattern pattern=Pattern.compile("^(\\+?\\+[0-9]{2})+[0-9]{10}$");
     public static void main(String[] args){
             System.out.println("welcome to regex program");
-            String lastName="neha1.xyz@bl.co.in";
-            Matcher matcher = pattern.matcher(lastName);
+            String MoNO="+918765432165";
+            Matcher matcher = pattern.matcher(MoNO);
             if (matcher.matches()) {
                 System.out.println("valid");
+                System.out.println(MoNO);
             } else {
                 System.out.println("invalid");
             }
-            System.out.println(lastName);
         }
     }
 
