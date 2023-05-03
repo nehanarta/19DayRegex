d986add5768d2cbed683b43c7aff967f23e4c9aa
Dell <nehabaswantrao@gmail.com>
2023-05-03 23:06:05 +0530

    Added:at least one Numberic

--- src/Bridglabz/User.java
+++ src/Bridglabz/User.java
@@ -5,7 +5,7 @@ public class User {
     static Pattern pattern = Pattern.compile("^[A-Za-z0-9.,@#$_%+-]{8}+$");
     public static void main(String[] args){
             System.out.println("welcome to regex program");
-            String pass="Dee9515@p";
+            String pass="Dee95$@p";
             Matcher matcher = pattern.matcher(pass);
             if (matcher.matches()) {
                 System.out.println("valid");
