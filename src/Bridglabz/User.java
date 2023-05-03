583b0506a078018101cbba4a3cb64968384590cd
Dell <nehabaswantrao@gmail.com>
2023-05-03 20:11:48 +0530

    Added:at least 1 one numeric

--- src/Bridglabz/User.java
+++ src/Bridglabz/User.java
@@ -5,7 +5,7 @@ public class User {
     static Pattern pattern = Pattern.compile("^[A-Za-z0-9.,@#$_%+-]{8}+$");
     public static void main(String[] args){
             System.out.println("welcome to regex program");
-            String pass="Deep95@p";
+            String pass="Dee9515@p";
             Matcher matcher = pattern.matcher(pass);
             if (matcher.matches()) {
                 System.out.println("valid");
