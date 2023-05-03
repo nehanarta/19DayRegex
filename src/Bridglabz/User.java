61edd6d5d06ab44627035ff65727049853a79400
Dell <nehabaswantrao@gmail.com>
2023-05-03 19:53:20 +0530

    Added:minimum 8 char

--- src/Bridglabz/User.java
+++ src/Bridglabz/User.java
@@ -2,17 +2,17 @@ package Bridglabz;
 import java.util.regex.*;
 
 public class User {
-    static Pattern pattern=Pattern.compile("^(.+)@(.+)$");
+    static Pattern pattern = Pattern.compile("^[a-zA-Z0-9.,@#$_%+-]{8}+$");
     public static void main(String[] args){
             System.out.println("welcome to regex program");
-            String lastName="neha1.xyz@bl.co.in";
-            Matcher matcher = pattern.matcher(lastName);
+            String pass="deeP95@p";
+            Matcher matcher = pattern.matcher(pass);
             if (matcher.matches()) {
                 System.out.println("valid");
             } else {
                 System.out.println("invalid");
             }
-            System.out.println(lastName);
+            System.out.println(pass);
         }
     }
 
